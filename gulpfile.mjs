82c4013fca2dbaa39e6800ace5a6b6535bf0a9b3
domiStaz <dominika.bryla@silnet.pl>
2023-07-11 12:02:38 +0200

    Commit #5

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,14 +1,24 @@
 import gulp from 'gulp';
-import sass from 'gulp-sass';
+import gulpSass from 'gulp-sass';
+import { deleteSync as del } from 'del';
+import sass from 'sass';
+const initedSass = gulpSass(sass);
+import plumber from 'gulp-plumber';
+
 
 gulp.task('sass', () => {
-    return gulp.src('./scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
+    return gulp.src('./sass/*.scss')
+        .pipe(plumber({ errorHandler: error }))
+        .pipe(initedSass())
         .pipe(gulp.dest('./css'));
 });
 
 gulp.task('watch', () => {
-    return gulp.watch('./scss/**/*.scss', gulp.series('sass'));
+    console.log('watch');
+    del(['./css/main.css']);
+    console.log('watch');
+    gulp.watch('./scss/*.scss', gulp.series('sass'));
 });
+
 
 gulp.task('default', gulp.series('watch'));
